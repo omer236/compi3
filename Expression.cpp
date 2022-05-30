051e3f9e79123d9718767449b56f49c0e3d7655d
omer236 <81875949+omer236@users.noreply.github.com>
2022-05-30 11:40:43 +0300

    Add files via upload

--- Expression.cpp
+++ Expression.cpp
@@ -2,13 +2,13 @@
 #include "MainProcess.h"
 #include <iostream>
 using namespace std;
+using namespace output;
 
 string getExpType(Expression* exp) {
     if (exp->type.size()!=0) {
         return exp->type;
     }
-    MainProcess& process = MainProcess::get_instance();
-    return process.getEntryInSymbolTable(exp->name)->type;
+    return MainProcess::get_instance().getEntryInSymbolTable(exp->name)->type;
 }
 
 void checkIfBoolUn(Expression* exp) {
@@ -16,13 +16,13 @@ void checkIfBoolUn(Expression* exp) {
     if (type == "BOOL") {
         return;
     }
-    output::errorMismatch(yylineno);
+    errorMismatch(yylineno);
     exit(1);
 }
 
 void checkIfBoolBin(Expression* exp1, Expression* exp2) {
     if(!(getExpType(exp2) == "BOOL"&&getExpType(exp1) == "BOOL")) {
-        output::errorMismatch(yylineno);
+        errorMismatch(yylineno);
         exit(1);
     }
 }
@@ -39,17 +39,16 @@ void checkByteSize(int size) {
     if (size < 256) {
         return;
     }
-    output::errorByteTooLarge(yylineno, to_string(size));
+    errorByteTooLarge(yylineno, to_string(size));
     exit(1);
 }
 
 void checkIfFuncAlreadyInSymbolTable(Expression* id) {
-    MainProcess& process = MainProcess::get_instance();
-    SymbolTableEntry* entry = process.getEntryInSymbolTable(id->name);
-    if (entry == nullptr) {
+    SymbolTableEntry* current_entry = MainProcess::get_instance().getEntryInSymbolTable(id->name);
+    if (current_entry == nullptr) {
         return;
     }
-    output::errorDef(yylineno, id->name);
+    errorDef(yylineno, id->name);
     exit(1);
 }
 
@@ -66,7 +65,7 @@ Expression* handleBinop(Expression* exp1, Expression* exp2) {
         return new Expression("", "INT");
     }
     else {
-        output::errorMismatch(yylineno);
+        errorMismatch(yylineno);
         exit(1);
     }
     return nullptr;
@@ -85,7 +84,7 @@ Expression* handleRelop(Expression* expression1, Expression* expression2) {
         return new Expression("", "BOOL");
     }
     else {
-        output::errorMismatch(yylineno);
+        errorMismatch(yylineno);
         exit(1);
     }
     return nullptr;
@@ -94,7 +93,7 @@ Expression* handleByte(Expression* expression) {
     if (stoi(expression->name) < 256) {
         return new Expression(expression->name, "BYTE");
     }
-    output::errorByteTooLarge(yylineno, expression->name);
+    errorByteTooLarge(yylineno, expression->name);
     exit(1);
 }
 
@@ -102,11 +101,11 @@ void handleCall(Expression* id, Expression* args) {
     MainProcess& process = MainProcess::get_instance();
     SymbolTableEntry* entry = process.getEntryInSymbolTable(id->name);
     if (entry == nullptr) {
-        output::errorUndefFunc(yylineno, id->name);
+        errorUndefFunc(yylineno, id->name);
         exit(1);
     }
-    else if (!entry->is_function) {
-        output::errorUndefFunc(yylineno, id->name);
+    else if (entry->is_function== false) {
+        errorUndefFunc(yylineno, id->name);
         exit(1);
     }
     SymbolTableEntryFunction* function_entry = dynamic_cast<class SymbolTableEntryFunction*>(entry);
@@ -114,27 +113,22 @@ void handleCall(Expression* id, Expression* args) {
     if (args != nullptr) {
         ExpressionFunction* args_list = dynamic_cast<class ExpressionFunction*>(args);
         if (args_list->arguments_names.size() != num_arguments) {
-            output::errorPrototypeMismatch(yylineno, function_entry->name, function_entry->arguments_types);
+            errorPrototypeMismatch(yylineno, function_entry->name, function_entry->arguments_types);
             exit(1);
         }
-
         reverse(args_list->arguments_names.begin(), args_list->arguments_names.end());
         reverse(args_list->arguments_types.begin(), args_list->arguments_types.end());
 
         for (int i = 0; i < num_arguments; i++) {
-            if (function_entry->arguments_types[i] != args_list->arguments_types[i])
+            if (function_entry->arguments_types[i] != args_list->arguments_types[i]&&!(((function_entry->arguments_types[i] == "INT") && (args_list->arguments_types[i] == "BYTE"))))
             {
-                if((function_entry->arguments_types[i] == "INT") && (args_list->arguments_types[i] == "BYTE"))
-                {
-                    continue;
-                }
-                output::errorPrototypeMismatch(yylineno, function_entry->name, function_entry->arguments_types);
+                errorPrototypeMismatch(yylineno, function_entry->name, function_entry->arguments_types);
                 exit(1);
             }
         }
     }
-    if (args == nullptr && num_arguments != 0) {
-        output::errorPrototypeMismatch(yylineno, function_entry->name, function_entry->arguments_types);
+    else if (!(num_arguments == 0)) {
+        errorPrototypeMismatch(yylineno, function_entry->name, function_entry->arguments_types);
         exit(1);
     }
 }
@@ -143,11 +137,11 @@ void checkID(Expression* id) {
     MainProcess& process = MainProcess::get_instance();
     SymbolTableEntry* entry = process.getEntryInSymbolTable(id->name);
     if (entry == nullptr) {
-        output::errorUndef(yylineno, id->name);
+        errorUndef(yylineno, id->name);
         exit(1);
     }
     else if (entry->is_function) {
-        output::errorUndef(yylineno, id->name);
+        errorUndef(yylineno, id->name);
         exit(1);
     }
 }
@@ -155,7 +149,7 @@ void checkID(Expression* id) {
 void addArgInDeclaration(Expression* args_list, Expression* new_arg) {
     ExpressionFunction* function_args_list = dynamic_cast<class ExpressionFunction*>(args_list);
     if (find(function_args_list->arguments_names.begin(), function_args_list->arguments_names.end(), new_arg->name) != function_args_list->arguments_names.end()) {
-        output::errorDef(yylineno, new_arg->name);
+        errorDef(yylineno, new_arg->name);
         exit(0);
     }
     function_args_list->arguments_types.push_back(new_arg->type);
